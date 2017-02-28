978540f34314786abf292611208a3258f54b9b0f
Blackbeard <blackbravo6@gmail.com>
2017-02-28 18:52:59 -0500

    Add files via upload

---
+++ MRE/media/lua/server/items/MRE_dist.lua
@@ -0,0 +1,68 @@
+require "Items/SuburbsDistributions";
+
+table.insert(SuburbsDistributions["bedroom"]["wardrobe"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["bedroom"]["wardrobe"].items, 3);
+table.insert(SuburbsDistributions["bedroom"]["sidetable"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["bedroom"]["sidetable"].items, 0.05);
+table.insert(SuburbsDistributions["all"]["locker"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["locker"].items, 100);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 1);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 1);
+table.insert(SuburbsDistributions["all"]["wardrobe"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["wardrobe"].items, 3);
+
+table.insert(SuburbsDistributions["all"]["crate"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["crate"].items, 3);
+table.insert(SuburbsDistributions["all"]["counter"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["counter"].items, 1);
+table.insert(SuburbsDistributions["all"]["sidetable"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["sidetable"].items, 1);
+table.insert(SuburbsDistributions["all"]["metal_shelves"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["all"]["metal_shelves"].items, 3);
+table.insert(SuburbsDistributions["shed"]["counter"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["shed"]["counter"].items, 4);
+table.insert(SuburbsDistributions["kitchen"]["counter"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["kitchen"]["counter"].items, 0.5);
+
+table.insert(SuburbsDistributions["spiffosstorage"]["all"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["spiffosstorage"]["all"].items, 4);
+table.insert(SuburbsDistributions["garagestorage"]["all"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["garagestorage"]["all"].items, 4);
+table.insert(SuburbsDistributions["policestorage"]["all"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["policestorage"]["all"].items, 3);
+table.insert(SuburbsDistributions["policestorage"]["all"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["policestorage"]["all"].items, 3);
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, 3);
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["policestorage"]["metal_shelves"].items, 3);
+
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["gunstore"]["counter"].items, 100);
+table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, 1);
+table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["gunstore"]["metal_shelves"].items, 100);
+
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 1);
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["gunstorestorage"]["all"].items, 100);
+table.insert(SuburbsDistributions["generalstore"]["other"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["generalstore"]["other"].items, 1);
+table.insert(SuburbsDistributions["storageunit"]["all"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["storageunit"]["all"].items, 3);
+table.insert(SuburbsDistributions["storageunit"]["all"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["storageunit"]["all"].items, 3);
+
+table.insert(SuburbsDistributions["hunting"]["metal_shelves"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["hunting"]["metal_shelves"].items, 3);
+table.insert(SuburbsDistributions["hunting"]["metal_shelves"].items, "MREFood.MREbox");
+table.insert(SuburbsDistributions["hunting"]["metal_shelves"].items, 3);
+table.insert(SuburbsDistributions["SafehouseLoot"]["counter"].items, "MREFood.MRE");
+table.insert(SuburbsDistributions["SafehouseLoot"]["counter"].items, 15);
+
